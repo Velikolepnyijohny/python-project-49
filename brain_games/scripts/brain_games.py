42e2b572336feda6c2679b368a545fa6c32599b8
Кузьмин Евгений <johnykozmen@yandex.ru>
2025-03-19 03:47:40 +0300

    /

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,3 +2,10 @@ def main():
     print("Welcome to the Brain Games!")
 if __name__ == "__main__":
     main()
+from brain_games.cli import welcome_user
+
+def main():
+    welcome_user()
+
+if __name__ == "__main__":
+    main()
